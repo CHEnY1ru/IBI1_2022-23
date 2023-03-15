086f47dd21e79f7cc836692b9db17d4ced483cdd
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-03-15 13:29:24 +0800

    Create variables.py

---
+++ Practice5/variables.py
@@ -0,0 +1,14 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=c-a
+if d>c:
+    print("the d is greter, and that shows Rob travel further to Los Angles")
+else:
+    print("the e is greter, and that shows Rob travel further to Haining")
+X=True
+Y=False
+W=X and Y
+Z=X or Y
+print(f"W is {W},Z is {Z}")
