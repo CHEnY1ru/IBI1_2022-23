3799fe3b0e736badbdabe766be40827a8f0f42de
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-03-15 14:01:50 +0800

    Create breed_like_rabbits.py

---
+++ Practice5/breed_like_rabbits.py
@@ -0,0 +1,9 @@
+#first set two variants, one is to judge the generation, one is to show the number of rabbits
+#then use the while loop to simulate the breeding of the rabbits until the number is over 100
+#print the result
+n=1
+k=2
+while k<100:
+    n+=1
+    k=2*k
+print(f"the generation {n} will over 100 rabbits ")
