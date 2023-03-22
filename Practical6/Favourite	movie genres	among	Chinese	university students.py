3eac63cabca5ff2e774ede4e9b262f23606d0178
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-03-22 15:15:27 +0800

    Update Favourite	movie genres	among	Chinese	university students.py

--- Practical6/Favourite	movie genres	among	Chinese	university students.py
+++ Practical6/Favourite	movie genres	among	Chinese	university students.py
@@ -1,19 +1,27 @@
-#set variables
-costs=[1,8,15,7,5,14,43,40]
-#sort
-sorted_costs=sorted(costs)
-print(sorted_costs)
-labels=["Los Angeles1984","Sydney2000","Atlanta1996","Seoul1988",
-        "Athens2003","Barcelona1992","London2012","Beijing 2008"]
-#import numpy and matplotlib
+#first import the numpy and matplotlib, and input the types of movie as labels,and the students number as sizes
+#then print a pie chart using matplotlib
+#creat a dictory to restore tha table,then print
+#craet a variabel as movie genre
+#check the genre form the dict and then print
+
 import matplotlib.pyplot as plt
 import numpy as np
-#change settings
-plt.figure(figsize=(10,5))
-plt.xticks(fontsize=6)
-plt.bar(labels,sorted_costs)
-plt.xlabel("olympic_location",color="red")
-plt.ylabel("COST")
-plt.title("Olympic_Costs",color="pink")
-#print barplot
+#set labels and sizes
+labels="comedy","Action" ,"Romance" ,"Fantasy" ,"Science-fiction","Horror","Crime","Documentary","History" ,"War"
+sizes=[73,42,38,28,22,19,18,12,8,7]
+#print a pie chart
+plt.pie(sizes,labels=labels,autopct='%1.1f%%')
 plt.show()
+#creat a dictory and set a varable as keyword 
+dic_movie={"comedy":73,"Action" :42,
+"Romance" :38,
+"Fantasy" :28,
+"Science-fiction": 22,
+"Horror": 19,
+"Crime": 18,
+"Documentary": 12,
+"History": 8,
+"War":7}
+print(dic_movie)
+movie_genre="War"#you can change the type
+print(dic_movie[movie_genre])
