a20ff459429a316af2ee95e1b7fa50b90a5c5a2b
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-04-12 16:11:09 +0800

    Create cds_counter.py

---
+++ Practical9/cds_counter.py
@@ -0,0 +1,11 @@
+import regex as re
+seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
+#input your DNA into this function and get the number of matched parts
+def count(seqs):
+    stop_codons=['TAA','TAG','TGA']
+    times=0
+    for i in stop_codons:
+        #use every condons in stop_condon to match the seq
+        times= times+len(re.findall(i,seqs, overlapped=True) ) 
+    print(times)
+count(seq)
