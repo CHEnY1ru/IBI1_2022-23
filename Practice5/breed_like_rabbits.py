9ae0f8eaedaf410e5def5e2496a167ac023cfd8f
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-05-20 22:39:14 +0800

    Update breed_like_rabbits.py

--- Practice5/breed_like_rabbits.py
+++ Practice5/breed_like_rabbits.py
@@ -1,9 +1,13 @@
-#first set two variates, one is to judge the generation, one is to show the number of rabbits
-#then use the while loop to simulate the breeding of the rabbits until the number is over 100
-#print the result
+'''pseudocode: first set two variates, one is to judge the generation, one is to show the number of rabbits
+then use the while loop to simulate the breeding of the rabbits until the number is over 100
+print the result'''
 n=1
 k=2
+#use while loop: while total number of rabbit less than 100 
 while k<100:
+    #generation plus 1
     n+=1
+    #rabbit continue to breed
     k=2*k
+#output result
 print(f"the generation {n} will over 100 rabbits ")
