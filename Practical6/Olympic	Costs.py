0944a4ae7e9dbddda75d5da5892d9ce38ddc7d37
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-05-21 15:28:32 +0800

    Update Olympic	Costs.py

--- Practical6/Olympic	Costs.py
+++ Practical6/Olympic	Costs.py
@@ -1,3 +1,7 @@
+'''Persudocode: it creates a bar plot using the matplotlib library in Python. 
+The code creates a dictionary with the location and cost data. 
+It then sorts the data by cost and creates two new lists with the sorted data. 
+Finally, it uses matplotlib to create a bar plot with the sorted data'''
 #create a dictionary
 location=['Los Angeles 1984','Seoul 1988','Barcelona 1992',
      'Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012']
@@ -8,7 +12,7 @@ zipped_cost=zip(costs,location)
 sorted_zip=sorted(zipped_cost)
 new_cost=[i[0] for i in sorted_zip]
 new_location=[i[1] for i in sorted_zip]
-print(new_location)
+print(new_cost)
 #import numpy and matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
