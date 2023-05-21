df4755f46483c506a7e4f5850590329813290339
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-05-21 15:35:50 +0800

    Update cds_counter.py

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,4 +1,4 @@
-import regex as re
+import re
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 #input your DNA into this function and get the number of matched parts
 def count(seqs):
@@ -6,6 +6,6 @@ def count(seqs):
     times=0
     for i in stop_codons:
         #use every condons in stop_condon to match the seq
-        times= times+len(re.findall(i,seqs, overlapped=True) ) 
+        times= times+len(re.findall("(?={0})".format(i),seqs) ) 
     print(times)
 count(seq)
