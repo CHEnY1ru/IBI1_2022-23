7efe42d6c2726ba3629e5082454f451dd6ec37cd
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-05-21 15:27:49 +0800

    Update Favourite	movie genres	among	Chinese	university students.py

--- Practical6/Favourite	movie genres	among	Chinese	university students.py
+++ Practical6/Favourite	movie genres	among	Chinese	university students.py
@@ -5,13 +5,6 @@
 #check the genre form the dict and then print
 
 import matplotlib.pyplot as plt
-import numpy as np
-#set labels and sizes
-labels="comedy","Action" ,"Romance" ,"Fantasy" ,"Science-fiction","Horror","Crime","Documentary","History" ,"War"
-sizes=[73,42,38,28,22,19,18,12,8,7]
-#print a pie chart
-plt.pie(sizes,labels=labels,autopct='%1.1f%%')
-plt.show()
 #creat a dictory and set a varable as keyword 
 dic_movie={"comedy":73,"Action" :42,
 "Romance" :38,
@@ -23,5 +16,13 @@ dic_movie={"comedy":73,"Action" :42,
 "History": 8,
 "War":7}
 print(dic_movie)
+
 movie_genre="War"#you can change the type
-print(dic_movie[movie_genre])
+print(f"number: {dic_movie[movie_genre]}")
+
+#set labels and sizes
+labels="comedy","Action" ,"Romance" ,"Fantasy" ,"Science-fiction","Horror","Crime","Documentary","History" ,"War"
+sizes=[73,42,38,28,22,19,18,12,8,7]
+#print a pie chart
+plt.pie(sizes,labels=labels,autopct='%1.1f%%')
+plt.show()
