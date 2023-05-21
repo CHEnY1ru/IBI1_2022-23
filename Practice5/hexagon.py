4551cf4ac6d0c0f5fc4fc7407690ad4b239fb34b
chen yiru <127192766+CHEnY1ru@users.noreply.github.com>
2023-05-21 15:25:09 +0800

    Update hexagon.py

--- Practice5/hexagon.py
+++ Practice5/hexagon.py
@@ -1,5 +1,6 @@
 #first create a tuple that contains 1 to 5, then use for loop to take out the number from the tuple
 n=(1,2,3,4,5)
 for i in n:
+    #use the formula
     k=2*i*(2*i-1)/2
     print(k)
